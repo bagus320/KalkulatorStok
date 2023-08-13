570e196781499f65b7a3bd038dc10fe0b340a274
bagus320 <bagustbr@gmail.com>
2023-08-13 16:24:05 +0700

    refactor some code

--- index.js
+++ index.js
@@ -1,48 +1,44 @@
 const button = document.querySelector("#button");
 const result = document.querySelector("#result");
-const boxAmount = document.querySelector("#boxAmount");
-const stripAmount = document.querySelector("#stripAmount");
-const tabAmount = document.querySelector("#tabAmount");
 
-let initialTab;
-let midTab;
+let tabletAwal;
+let tabletBerkurang;
 let finalBox;
 let finalStrip;
 let finalTab;
 
 function calc() {
-  const initialValue = document.querySelector("#initialValue").value;
-  const midValue = document.querySelector("#midValue").value;
-  const firstSelector = document.querySelector("#firstSelector").value;
-  const secondSelector = document.querySelector("#secondSelector").value;
+  // mengambil nilai yang di inputkan oleh user
+  let jumlahAwal = document.querySelector("#initialValue").value;
+  let jumlahBerkurang = document.querySelector("#midValue").value;
+  // mengambil nilai dari form selector
+  const selectorPertama = document.querySelector("#firstSelector").value;
+  const selectorKedua = document.querySelector("#secondSelector").value;
 
-  switch (firstSelector) {
+  switch (selectorPertama) {
     case "inBox":
-      initialTab = initialValue * 100;
+      jumlahAwal *= 100;
       break;
     case "inStrip":
-      initialTab = initialValue * 10;
+      jumlahAwal *= 10;
       break;
     default:
-      initialTab = initialValue;
+      jumlahAwal;
   }
-  switch (secondSelector) {
+  switch (selectorKedua) {
     case "inBox":
-      midTab = midValue * 100;
+      jumlahBerkurang *= 100;
       break;
     case "inStrip":
-      midTab = midValue * 10;
+      jumlahBerkurang *= 10;
       break;
     default:
-      midTab = midValue;
+      jumlahBerkurang;
   }
-  let finalBox = Math.floor((initialTab - midTab) / 100);
-  let finalStrip = Math.floor((initialTab - midTab) / 10);
-  let finalTab = Math.floor(initialTab - midTab);
+  let finalBox = Math.floor((jumlahAwal - jumlahBerkurang) / 100);
+  let finalStrip = Math.floor((jumlahAwal - jumlahBerkurang) / 10);
+  let finalTab = Math.floor(jumlahAwal - jumlahBerkurang);
 
-  boxAmount.innerHTML = "Sisa Jumlah Obat Dalam Box: " + finalBox;
-  stripAmount.innerHTML = "Sisa Jumlah Obat Dalam Strip: " + finalStrip;
-  tabAmount.innerHTML = "Sisa Jumlah Obat Dalam Tablet: " + finalTab;
+  result.innerHTML = `Sisa Jumlah Obat Dalam Box: ${finalBox} <br> Sisa Jumlah Obat Dalam Strip: ${finalStrip} <br> Sisa Jumlah Obat Dalam Tablet ${finalTab}`;
 }
-
 button.addEventListener("click", calc);
